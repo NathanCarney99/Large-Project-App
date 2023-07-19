c6375f7599e4ebfcd49ce5a2a70944f704d00ecb
Nathan <nathan.carney@knights.ucf.edu>
2023-07-19 16:41:46 -0400

    completed about page

--- pawhub-app/Navigation/screens/Contact.js
+++ pawhub-app/Navigation/screens/Contact.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Contact({navigation}) {
   return (
     <View>
-      <Text onPress={() => navigation.navigate('Home')}>Contact</Text>
+      
     </View>
   )
 }
