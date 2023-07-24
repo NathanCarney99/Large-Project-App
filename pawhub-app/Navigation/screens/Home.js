828d9c83c6c7fbbc2acbb07dcad82278209103d1
Nathan <nathan.carney@knights.ucf.edu>
2023-07-24 14:15:06 -0400

    removed search bar

--- pawhub-app/Navigation/screens/Home.js
+++ pawhub-app/Navigation/screens/Home.js
@@ -13,15 +13,7 @@ export default function Home({ navigation }) {
 
   return (
     <ScrollView>
-      <View style={styles.blackContainer}>
-        <TouchableOpacity onPress={handleSearchUsers}>
-          <View style={styles.searchContainer}>
-            {/* Add the Ionicons component to display the search icon */}
-            <Ionicons name="search" size={24} color="#025464" style={styles.searchIcon} />
-            <Text style={styles.searchButton}>Search Users</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+      
 
       <View>
         <NewPost />
@@ -31,26 +23,4 @@ export default function Home({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({
-  blackContainer: {
-    backgroundColor: '#025464',
-  },
-  searchContainer: {
-    padding: 10,
-    backgroundColor: 'white',
-    paddingHorizontal: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 60,
-    margin: 10,
-    flexDirection: 'row', // Add flexDirection: 'row' to align icon and text horizontally
-  },
-  searchIcon: {
-    marginRight: 5, // Adjust the margin between the icon and text
-  },
-  searchButton: {
-    color: '#025464',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
+
